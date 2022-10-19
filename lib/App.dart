dea10f73918b0de506c2a7fd14c59c24c34156bb
Gabriel Rotariu <dam.gabriel.rotariu@gmail.com>
2022-10-19 12:08:55 +0200

    Primer commit: Inicio de la applicación, dependencias con Firebase

---
+++ lib/App.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+return MaterialApp(
+
+initialRoute: "",
+
+
+);
+
+
+  }}
